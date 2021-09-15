a91befd806878740d6a51675553e36222abec57a
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-09-15 21:53:41 +0800

    Add module exports example

---
+++ Uncategorized/nodeModuleExample.js
@@ -0,0 +1,60 @@
+// @ts-nocheck
+
+var files = {
+  aModule: aModule,
+  bModule: bModule,
+  cModule: cModule,
+  dModule: dModule,
+};
+
+var cachedModules = {};
+function require(fileName) {
+  if (cachedModules[fileName]) {
+    return cachedModules[fileName];
+  }
+
+  // 分配物件記憶體位置
+  var exports = {};
+  var module = { exports };
+
+  files[fileName](require, exports, module);
+  cachedModules[fileName] = module.exports;
+  return module.exports;
+}
+
+function aModule(require, exports, module) {
+  var randomValue = Math.random(); // 用 random 測試快取機制
+
+  exports2 = exports; // 原本同一個 reference
+  console.log(exports === exports2) // true
+  // exports2.value = randomValue;
+  exports2 = { value: randomValue }; // 若 exports2 賦予一個新的物件，就會分配新的記憶體，使得 reference 不同
+  console.log(exports === exports2) // false
+  console.log("a:", module); // 結果 a: { exports: {} }
+}
+
+function bModule(require, exports, module) {
+  var randomValue = Math.random();
+
+  exports.value = randomValue;
+  console.log("b:", module); // 結果 b: { exports: value: [randomValue] }
+}
+
+function cModule(require, exports, module) {
+  var valueA = require("aModule").value;
+  var valueB = require("bModule").value;
+
+  console.log("c(a):", valueA);
+  console.log("c(b):", valueB);
+}
+
+// 其中若想修改 module
+function dModule(require, exports, module) {
+  require("cModule");
+  var b = require("bModule");
+  console.log("d(original B):", b);
+  b.value = "overwrite";
+  console.log("d(overwritten B):", b.value);
+}
+
+require("dModule");
